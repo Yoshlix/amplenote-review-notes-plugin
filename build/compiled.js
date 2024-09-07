e6acef0e78252b9a50b3b8fb4b6a2d00422408a7
Simon K. <75274054+Yoshlix@users.noreply.github.com>
2024-09-07 18:32:58 +0200

    test11.1

--- build/compiled.js
+++ build/compiled.js
@@ -3,7 +3,7 @@
   var plugin = {
     dailyJotOption: {
       check(app, noteHandle) {
-        if (app.settings["LastDayReviewed"] == (/* @__PURE__ */ new Date()).toLocaleDateString()) {
+        if (app.settings["LastDayReviewed"] === (/* @__PURE__ */ new Date()).toLocaleDateString()) {
           return false;
         }
         return "Review notes";
@@ -12,6 +12,10 @@
         app.alert("Review notes");
         await app.setSetting("LastDayReviewed", (/* @__PURE__ */ new Date()).toLocaleDateString());
       }
+    },
+    async noteOption(app, noteUUID) {
+      const markdown = await app.getNoteContent({ uuid: noteUUID });
+      app.alert(markdown);
     }
     // There are several other entry points available, check them out here: https://www.amplenote.com/help/developing_amplenote_plugins#Actions
     // You can delete any of the insertText/noteOptions/replaceText keys if you don't need them
